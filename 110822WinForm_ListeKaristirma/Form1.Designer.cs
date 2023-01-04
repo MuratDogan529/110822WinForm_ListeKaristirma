f11ddec60be6ed084acfbc8e67028ee1cee7a5d4
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:32:52 +0300

    Add project files.

---
+++ 110822WinForm_ListeKaristirma/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _110822WinForm_ListeKaristirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnKarıstır = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(301, 7);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(118, 27);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(301, 62);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(118, 27);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnKarıstır
+            // 
+            this.btnKarıstır.Location = new System.Drawing.Point(301, 120);
+            this.btnKarıstır.Name = "btnKarıstır";
+            this.btnKarıstır.Size = new System.Drawing.Size(118, 27);
+            this.btnKarıstır.TabIndex = 2;
+            this.btnKarıstır.Text = "Karıştırma";
+            this.btnKarıstır.UseVisualStyleBackColor = true;
+            this.btnKarıstır.Click += new System.EventHandler(this.btnKarıstır_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "İsim:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(-1, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 276);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 316);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKarıstır);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnKarıstır;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 110822WinForm_ListeKaristirma/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _110822WinForm_ListeKaristirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+           
+            string isim = textBox1.Text;
+            if(listBox1.Items.Contains(isim)==true)
+            {
+                MessageBox.Show("Girilen isim listede vardır.Lütfen farklı bir isim giriniz.");
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+            else
+            {
+                listBox1.Items.Add(isim);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+            
+        }
+
+        private void btnKarıstır_Click(object sender, EventArgs e)
+        {
+            string[] isimler =new string[listBox1.Items.Count];
+            Random rnd = new Random();
+
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int sayi = rnd.Next(0,listBox1.Items.Count);
+                string rastgele = listBox1.Items[sayi].ToString();
+                
+                if(isimler.Contains(rastgele)==false)
+                {
+                    isimler[i] = rastgele;
+                }
+                else
+                {
+                    i--;         
+                }
+                
+            }
+            listBox1.Items.Clear();
+            foreach (string item in isimler)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+           
+            DialogResult sonuc= MessageBox.Show("Silmek istediğinize emin misiniz?(Are you sure delete)", "Silme Formu",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(sonuc==DialogResult.Yes)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            
+
+        }
+    }
+}
